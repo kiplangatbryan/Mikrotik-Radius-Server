8fa9e58b76781c3b20397cbca9dbccda087cecca
kiplangatbryan <kiplangatsgt@gmail.com>
2022-03-15 12:43:47 +0300

    chances

--- src/routes/index.js
+++ src/routes/index.js
@@ -128,10 +128,12 @@ router.get('/StalePayment/:mac_addr',async (req, res) =>{
 
 	console.log(customDate(user.time_signed, chosen_bundle[0].limit))
 
-	if (customDate(user.time_signed, chosen_bundle[0].limit) > new Date(Date.now())){
+	if (customDate(user.time_signed, chosen_bundle[0].limit) < new Date(Date.now())){
 		console.log('sth')
 		return res.json({status: 'validated', user: { username: user.userName, passwd: user.passwd}})
 	}
+
+	console.log('failed')
 	return res.json({status: 'failed'})
 
 
