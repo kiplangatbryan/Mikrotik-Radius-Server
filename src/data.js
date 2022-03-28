40768b2ed4885183deb3304b3a96e068223872e2
bryan <55278664+kiplangatbryan@users.noreply.github.com>
2022-03-28 21:56:36 +0300

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const bundle_offer = [
+    {   
+        name: 'okoa_bundle', 
+        value: 5, 
+        limit: 1
+    }, 
+    { 
+        name: 'simple_bundle', 
+        value: 10, 
+        limit: 2
+    }, 
+    { 
+        name: 'jenga_bundle', 
+        value: 20, 
+        limit: 5
+    }, 
+    { 
+        name: 'fire_bundle', 
+        value: 25, 
+        limit: 7
+    }, 
+    { 
+        name: 'blaze_bundle', 
+        value: 30, 
+        limit: 12
+    },
+    { 
+        name: 'dedicated', 
+        value: 40, 
+        limit: 24
+    }  
+] 
+
+module.exports = bundle_offer
