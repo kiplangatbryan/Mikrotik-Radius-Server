57043090daf75596c9850b809ee08af88056aea0
bryan <55278664+kiplangatbryan@users.noreply.github.com>
2022-03-28 21:55:04 +0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,38 +6,7 @@ const assert = require('assert')
 
 const fs = require('fs')
 
-const bundle_offer = [
-    {   
-        name: 'okoa_bundle', 
-        value: 5, 
-        limit: 1
-    }, 
-    { 
-        name: 'simple_bundle', 
-        value: 10, 
-        limit: 2
-    }, 
-    { 
-        name: 'jenga_bundle', 
-        value: 20, 
-        limit: 5
-    }, 
-    { 
-        name: 'fire_bundle', 
-        value: 25, 
-        limit: 7
-    }, 
-    { 
-        name: 'blaze_bundle', 
-        value: 30, 
-        limit: 12
-    },
-    { 
-        name: 'dedicated', 
-        value: 40, 
-        limit: 24
-    }  
-] 
+const { bundle_offer } = require("../data.js") 
 
 const { triggerWebPay, WebPayCb } = require('../api')
 
