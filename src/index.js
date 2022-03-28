8780dc65f4ae29830e7a9128976dd4565f29dd61
bryan <55278664+kiplangatbryan@users.noreply.github.com>
2022-03-28 18:07:57 +0300

    Update index.js
    minor refactoring

--- src/index.js
+++ src/index.js
@@ -12,33 +12,15 @@ const { DataReset } = require('./api')
 const PORT = process.env.PORT || 3000 
 const app = express()
 
-
-
 // common settings
 app.set("view engine", "ejs");
 app.set("views", "src/pages");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use('/static', express.static(path.join(__dirname, '..', 'static')))
 app.use(cors())
 
 app.use(my_routes)
-
-app.use('', (req, res, next) =>{
-  // if (!req.session.user) {
-  //   return next();
-  // }
-  // try {
-  //   const user = await User.findById(req.session.user._id);
-  //   req.user = user;
-  //   next();
-  // } catch (err) {
-  //   const error = new Error(err);
-  //   error.statusCode = 500;
-  //   next(error);
-  // }
-})
 
 app.use('*', (req, res)=>{
     return res.status(404).render('404')
